0b9f71edf9efaa27d08a4f0b70fda1ad736d5097
lucas <lucas@DESKTOP-8VJP0R2>
2023-04-07 20:29:24 -0300

    Instruções melhoradas

--- scripts/estudo2.js
+++ scripts/estudo2.js
@@ -20,11 +20,13 @@ lista.forEach(function(item){
 //Utilizando do CSS em JS
 //Definir o elemento pra ser alterado, para alterar as informações de um dos elementos do CSS basta usar o comando:
 //document.getElementById('NOMEDOELEMENTO'), você também pode setar em uma variavel CONST o valor para que facilite a manipulação do CSS
-const elemento = document.getElementById('meuElemento'); // definindo o elemento na variavel elemento
+const elemento = document.getElementById('title'); // definindo o elemento na variavel elemento
 elemento.style.fontFamily = 'Georgia'; // definindo a fonte para "georgia usando o back-end"
 elemento.style.textAlign = 'center'; // Definindo o texto pra aparecer no centro da tela.
 //ou você pode utilizar diretamente caso você queira fazer uma alteração pequena em um elemento especifico
 document.getElementById('meuElemento2').style.fontStyle = "italic";//Mudando o estilo da fonte para itálico!
+
+
 
 
 

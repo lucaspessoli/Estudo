16bbf6b1c425a6c7884ea4603218d9f1b76e1148
Lucas Pessoli <pessolilucas@gmail.com>
2023-07-16 16:04:19 -0300

    ADD Pasta Angular Pipe Operators estudo 9

---
+++ Angular/9 - Pipe Operators/components/pipes/pipes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.css']
+})
+export class PipesComponent {
+
+  nomeCidadao: string = "marcos joao da silva"
+  data = new Date();
+
+}
